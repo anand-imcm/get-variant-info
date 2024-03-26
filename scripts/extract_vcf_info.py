72495228d41e4cf0c385cefd6b1c92cadaf43b41
anand-imcm <anand.maurya@well.ox.ac.uk>
2024-03-26 16:17:48 +0000

    Refactor extract_vcf_info function to handle multiple extract options

--- scripts/extract_vcf_info.py
+++ scripts/extract_vcf_info.py
@@ -25,40 +25,51 @@ import argparse
 import os
 import pandas as pd
 
-def extract_vcf_info(vcf_file, extract, output_file):
+def extract_vcf_info(vcf_file, extract):
     """
     Extracts Genotype (GT), Estimated Alternate Allele Dosage (DS) or Estimated Posterior Probabilities for Genotypes 0/0, 0/1 and 1/1 (GP) information from a VCF file and writes it to a CSV file.
 
     Args:
         vcf_file (str): Path to the VCF file.
-        output_file (str): Path to the output file.
+        extract (str): Choices are "GT", "DS", "GP"
 
     Returns:
         None
     """
     print(f"Parsing file: {vcf_file}")
-    with pysam.VariantFile(vcf_file) as vcf:
-        with open(output_file, 'w') as out:
-            sample_names = vcf.header.samples
-            out.write("IID\t" + '\t'.join(sample_names) + "\n")
-            for record in vcf:
-                chrom = "NA"
-                pos = "NA"
-                ref = "NA"
-                alt = "NA"
-                info = []
-                if record.chrom and record.pos and record.ref and record.alts:
-                    chrom = record.chrom
-                    pos = record.pos
-                    ref = record.ref
-                    alt = ','.join(map(str, record.alts))
-                if extract == 'GT':
-                    info = ['|'.join(map(str, sample['GT'])) if sample['GT'] is not None else 'NA' for sample in record.samples.values()]
-                elif extract == 'DS':
-                    info = [str(int(sample['DS'])) if sample['DS'].is_integer() else '{:.3f}'.format(sample['DS']) if sample['DS'] is not None else 'NA' for sample in record.samples.values()]
-                elif extract == 'GP':
-                    info = ['|'.join(str(int(value)) if value.is_integer() else f'{value:.3f}' for value in sample['GP']) if sample['GP'] is not None else 'NA' for sample in record.samples.values()]
-                out.write(f"{chrom}:{pos}:{ref}:{alt}\t" + '\t'.join(info) + "\n")
+    for option in extract:
+        output_file_temp = f"{args.out}_{option}_temp.txt"
+        with pysam.VariantFile(vcf_file) as vcf:
+            with open(output_file_temp, 'w') as out:
+                sample_names = vcf.header.samples
+                out.write("IID\t" + '\t'.join(sample_names) + "\n")
+                for record in vcf:
+                    chrom = "NA"
+                    pos = "NA"
+                    ref = "NA"
+                    alt = "NA"
+                    info = []
+                    if record.chrom and record.pos and record.ref and record.alts:
+                        chrom = record.chrom
+                        pos = record.pos
+                        ref = record.ref
+                        alt = ','.join(map(str, record.alts))
+                    if option == 'GT':
+                        info = ['|'.join(map(str, sample['GT'])) if sample['GT'] is not None else 'NA' for sample in record.samples.values()]
+                        out.write(f"{chrom}:{pos}:{ref}:{alt}\t" + '\t'.join(info) + "\n")
+                    elif option == 'DS':
+                        info = [str(int(sample['DS'])) if sample['DS'].is_integer() else '{:.3f}'.format(sample['DS']) if sample['DS'] is not None else 'NA' for sample in record.samples.values()]
+                        out.write(f"{chrom}:{pos}:{ref}:{alt}\t" + '\t'.join(info) + "\n")
+                    elif option == 'GP':
+                        info = ['|'.join(str(int(value)) if value.is_integer() else f'{value:.3f}' for value in sample['GP']) if sample['GP'] is not None else 'NA' for sample in record.samples.values()]
+                        out.write(f"{chrom}:{pos}:{ref}:{alt}\t" + '\t'.join(info) + "\n")
+        # transposed file:
+        df = pd.read_csv(output_file_temp, sep='\t', index_col=0)
+        df_transposed = df.transpose().reset_index()
+        df_transposed.rename(columns={'index': 'IID'}, inplace=True)
+        df_transposed.to_csv(f"{args.out}_{option}.csv", sep=',', index=False)
+        os.remove(output_file_temp)
+        print(f"Output generated: {args.out}_{option}.csv")
 
 
 if __name__ == "__main__":
@@ -68,16 +79,17 @@ if __name__ == "__main__":
         formatter_class=argparse.RawTextHelpFormatter
     )
     parser.add_argument('--vcf', help='The VCF file to parse.', required=True)
-    parser.add_argument('--extract', help='The type of information to extract. Choices are "GT", "DS", "GP".', required=True, choices=['GT', 'DS', 'GP'], )
+    # parser.add_argument('--extract', help='The type of information to extract. Choices are "GT", "DS", "GP".', required=True, choices=['GT', 'DS', 'GP'], )
+    parser.add_argument('--extract', help='The type of information to extract. Choices are "GT", "DS", "GP".', required=True, choices=['GT', 'DS', 'GP'], nargs='+')
     parser.add_argument('--out', help='Provide the name of output file (including the file path)', required=True)
     args = parser.parse_args()
-    output_file_temp = f"{args.out}.temp.txt"
-    extract_vcf_info(args.vcf, args.extract, output_file_temp)
+    # output_file_temp = f"{args.out}.temp.txt"
+    extract_vcf_info(args.vcf, args.extract)
 
-    # transposed file:
-    df = pd.read_csv(output_file_temp, sep='\t', index_col=0)
-    df_transposed = df.transpose().reset_index()
-    df_transposed.rename(columns={'index': 'IID'}, inplace=True)
-    df_transposed.to_csv(args.out, sep=',', index=False)
-    os.remove(output_file_temp)
-    print(f"Output generated: {args.out}")
+    # # transposed file:
+    # df = pd.read_csv(output_file_temp, sep='\t', index_col=0)
+    # df_transposed = df.transpose().reset_index()
+    # df_transposed.rename(columns={'index': 'IID'}, inplace=True)
+    # df_transposed.to_csv(args.out, sep=',', index=False)
+    # os.remove(output_file_temp)
+    # print(f"Output generated: {args.out}")
